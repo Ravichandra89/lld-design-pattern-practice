4408ae419fd6ec47687c8e0dea73a541a8ca530a
Ravi chandra <githubravi01@gmail.com>
2025-08-06 12:43:09 +0530

    Legacy::Jpeg_Compressor

--- Problem2/legacyCompressors/JpegCompressor.hpp
+++ Problem2/legacyCompressors/JpegCompressor.hpp
@@ -4,6 +4,15 @@
 #ifndef JPEG_LEGACY_COMPRESS_HPP
 #define JPEG_LEGACY_COMPRESS_HPP
 
+#include <iostream>
+#include <string>
+using namespace std;
 
+class JpegCompressor {
+    public: 
+        void JpegCompress(const string& inPath, const string& outPath) {
+            cout << "Legacy JPEG Compressor " << inPath << " -> " << outPath << endl;
+        }
+};
 
 #endif
